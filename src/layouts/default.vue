8683db3daf2a6b0fd2632cedd9d2cb7ac17e9916
Shotaro Hirose <shotaro199959@icloud.com>
2021-05-01 23:18:20 +0900

    Add: ヘッダーを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app>
+    <v-app-bar flat fixed app>
+      <nuxt-link :to="{ name: 'index' }">
+        <h2 style="color: #6200ea">RUNTEQ POLL</h2>
+      </nuxt-link>
+    </v-app-bar>
     <v-main>
       <v-container fluid fill-height>
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
       <span class="ml-2">runteq poll</span>
     </v-footer>
@@ -19,3 +24,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
